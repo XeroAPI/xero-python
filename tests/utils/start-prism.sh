4e8b1af5f359bef0601582e82e8684d13b25f205
Ryan Duguid <152749594+ryanduguid@users.noreply.github.com>
2026-08-13 03:18:36 +1000

    ci: pin OpenAPI mock inputs

--- tests/utils/start-prism.sh
+++ tests/utils/start-prism.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-branchName=${1:-"master"}
+specRef=${1:?pass an immutable Xero-OpenAPI commit SHA}
+if ! [[ "$specRef" =~ ^[0-9a-f]{40}$ ]]; then
+  echo "Xero-OpenAPI ref must be a full commit SHA" >&2
+  exit 2
+fi
 
-prism mock https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/refs/heads/$branchName/xero_accounting.yaml --host 127.0.0.1 --port 4010 &
-prism mock https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/refs/heads/$branchName/xero-app-store.yaml --host 127.0.0.1 --port 4011 &
-prism mock https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/refs/heads/$branchName/xero_assets.yaml --host 127.0.0.1 --port 4012 &
-prism mock https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/refs/heads/$branchName/xero_bankfeeds.yaml --host 127.0.0.1 --port 4013 &
-prism mock https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/refs/heads/$branchName/xero-finance.yaml --host 127.0.0.1 --port 4014 &
-prism mock https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/refs/heads/$branchName/xero-payroll-uk.yaml --host 127.0.0.1 --port 4015 &
-prism mock https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/refs/heads/$branchName/xero-payroll-nz.yaml --host 127.0.0.1 --port 4016 &
-prism mock https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/refs/heads/$branchName/xero-payroll-au.yaml --host 127.0.0.1 --port 4017 &
-prism mock https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/refs/heads/$branchName/xero-projects.yaml --host 127.0.0.1 --port 4018
+specUrl="https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/$specRef"
+
+prism mock "$specUrl/xero_accounting.yaml" --host 127.0.0.1 --port 4010 &
+prism mock "$specUrl/xero-app-store.yaml" --host 127.0.0.1 --port 4011 &
+prism mock "$specUrl/xero_assets.yaml" --host 127.0.0.1 --port 4012 &
+prism mock "$specUrl/xero_bankfeeds.yaml" --host 127.0.0.1 --port 4013 &
+prism mock "$specUrl/xero-finance.yaml" --host 127.0.0.1 --port 4014 &
+prism mock "$specUrl/xero-payroll-uk.yaml" --host 127.0.0.1 --port 4015 &
+prism mock "$specUrl/xero-payroll-nz.yaml" --host 127.0.0.1 --port 4016 &
+prism mock "$specUrl/xero-payroll-au.yaml" --host 127.0.0.1 --port 4017 &
+prism mock "$specUrl/xero-projects.yaml" --host 127.0.0.1 --port 4018
